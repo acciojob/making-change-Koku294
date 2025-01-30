e3382918ba788e0f52cf1d00fc2598d0bd758917
Koku294 <90713556+Koku294@users.noreply.github.com>
2025-01-30 22:42:45 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,29 @@
 const makeChange = (c) => {
   // your name here
+	  let change = {
+    q: 0, // Quarters
+    d: 0, // Dimes
+    n: 0, // Nickels
+    p: 0  // Pennies
+  };
+
+  // Calculate number of quarters
+  change.q = Math.floor(c / 25);
+  c %= 25; // Remainder after quarters
+
+  // Calculate number of dimes
+  change.d = Math.floor(c / 10);
+  c %= 10; // Remainder after dimes
+
+  // Calculate number of nickels
+  change.n = Math.floor(c / 5);
+  c %= 5; // Remainder after nickels
+
+  // Calculate number of pennies
+  change.p = c; // What's left is the number of pennies
+
+  return change;
+
 };
 
 // Do not the change the code below
